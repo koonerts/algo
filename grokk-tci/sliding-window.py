08f82b42b73a5720836f026b90d8969f4b7895d9
Taranjit Kooner <koonerts@gmail.com>
2020-11-14 22:52:40 -0500

    progress

--- grokk-tci/sliding-window.py
+++ grokk-tci/sliding-window.py
@@ -184,4 +184,34 @@ def non_repeat_substring(str: str) -> int:
     return max_
 
 
+def length_of_longest_substring(str: str, k: int):
+    """
+    Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’ letters with any letter,
+    find the length of the longest substring having the same letters after replacement.
+
+    Example 1:
+    Input: String="aabccbb", k=2
+    Output: 5
+    Explanation: Replace the two 'c' with 'b' to have a longest repeating substring "bbbbb".
+
+    Example 2:
+    Input: String="abbcb", k=1
+    Output: 4
+    Explanation: Replace the 'c' with 'b' to have a longest repeating substring "bbbb".
+
+    Example 3:
+    Input: String="abccde", k=1
+    Output: 3
+    Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+    """
+
+    start, next, max_ = 0, 0, 0
+    start_char = str[start]
+    char_map = {}
+
+    for i, c in enumerate(str):
+        if c != start_char:
+            char_map[c] = (char_map.get(c) or 0) + 1
+
+
 print(non_repeat_substring('abccde'))
