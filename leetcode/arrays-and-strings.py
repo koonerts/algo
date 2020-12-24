4ad17fcbe431ed15ad97ade0a643893d43f3dd09
Taranjit Kooner <koonerts@gmail.com>
2020-12-24 15:18:59 -0600

    .

--- leetcode/arrays-and-strings.py
+++ leetcode/arrays-and-strings.py
@@ -69,7 +69,7 @@ class Solution:
 
         return a
 
-    def sortedSquares(self, A: list[int]) -> list[int]:
+    def sortedSquares(self, a: list[int]) -> list[int]:
         """
         Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
         Input: [-4,-1,0,3,10]
@@ -78,23 +78,23 @@ class Solution:
 
         ret = []
         l, r = 0, 0
-        while r < len(A) and A[r] < 0:
+        while r < len(a) and a[r] < 0:
             r += 1
         l = r-1
 
-        while len(ret) != len(A):
-            if l < 0 or l >= len(A):
-                ret.append(A[r] ** 2)
+        while len(ret) != len(a):
+            if l < 0 or l >= len(a):
+                ret.append(a[r] ** 2)
                 r += 1
-            elif r < 0 or r >= len(A):
-                ret.append(A[l] ** 2)
+            elif r < 0 or r >= len(a):
+                ret.append(a[l] ** 2)
                 l -= 1
             else:
-                if abs(A[l]) <= abs(A[r]):
-                    ret.append(A[l] ** 2)
+                if abs(a[l]) <= abs(a[r]):
+                    ret.append(a[l] ** 2)
                     l -= 1
                 else:
-                    ret.append(A[r] ** 2)
+                    ret.append(a[r] ** 2)
                     r += 1
 
         return ret
@@ -194,28 +194,6 @@ class Solution:
                 nums[new_index] *= -1
 
         return [i+1 for i in range(len(nums)) if nums[i] > 0]
-
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s:
-            return 0
-        elif len(s) == 1:
-            return 1
-
-        start = 0
-        curr_substring_len, max_substr_len = 1, 1
-        char_set = {s[0]}
-
-        for i in range(1, len(s)):
-
-            while s[i] in char_set:
-                curr_substring_len -= 1
-                char_set.remove(s[start])
-                start += 1
-
-            curr_substring_len += 1
-            char_set.add(s[i])
-            max_substr_len = max(max_substr_len, curr_substring_len)
-        return max_substr_len
 
     def singleNumber(self, nums: list[int]) -> int:
         xor = 0
@@ -323,25 +301,6 @@ class Solution:
 
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         pass
-
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s: return 0
-
-        max_ss = 1
-        char_freq = {s[0]: 1}
-        left, right = 0, 1
-        while right < len(s):
-            char_freq[s[right]] = char_freq.get(s[right], 0)+1
-            while char_freq[s[right]] > 1:
-                if char_freq[s[left]] == 1:
-                    del char_freq[s[left]]
-                else:
-                    char_freq[s[left]] -= 1
-                left += 1
-            max_ss = max(right-left+1, max_ss)
-            right += 1
-        print(max_ss)
-        return max_ss
 
     def longestPalindrome(self, s: str) -> str:
         left, right = 0, 0
@@ -485,5 +444,199 @@ class Solution:
 
         return 0 if not(-2**31-1 <= reversed_x <= 2**31-1) else reversed_x
 
+    def firstUniqChar(self, s: str) -> int:
+        char_map = {}
+        for i, c in enumerate(s):
+            char_map[c] = char_map.get(c, []) + [i]
 
-print(Solution().reverse(-12301))
+        for key, val in char_map.items():
+            if len(val) == 1:
+                return val[0]
+
+        return -1
+
+    def isPalindrome(self, s: str) -> bool:
+        if s == '': return True
+
+        start, end = 0, len(s) - 1
+        while start <= end:
+            while (not s[start].isalnum()) and start < end:
+                start += 1
+            while (not s[end].isalnum()) and start < end:
+                end -= 1
+
+            if s[start].casefold() != s[end].casefold():
+                return False
+
+            start += 1
+            end -= 1
+        return True
+
+    def myAtoi(self, s: str) -> int:
+        if not s: return 0
+
+        start, end = -1, -1
+        for i, c in enumerate(s):
+            if c in ['+', '-'] or c.isnumeric():
+                start = i
+                end = i+1
+                while end < len(s) and s[end].isnumeric():
+                    end += 1
+                break
+            elif c.isspace():
+                continue
+            else:
+                return 0
+
+        sign = '+'
+
+        if s[start] == '+':
+            start += 1
+            if s[start] == '-':
+                sign = '-'
+                start += 1
+        elif s[start] == '-':
+            sign = '-'
+            start += 1
+            if s[start] == '-':
+                sign = '-'
+                start += 1
+
+        if not s[start:end].isnumeric():
+            return 0
+
+        val = int(s[start:end])
+        if val > 2**31:
+            return 2**31
+        elif val < (-2)**31:
+            return (-2)**31
+        else:
+            return val
+
+    def strStr(self, haystack: str, needle: str) -> int:
+        pass
+
+    def minWindow(self, s: str, t: str) -> str:
+        left, right = 0, 0
+        char_freq = {}
+
+        ret_str = ''
+        while right < len(s):
+            if s[right] in t:
+                char_freq[s[right]] = char_freq.get(s[right], 0) + 1
+
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1_list = list(map(int, version1.split('.')))
+        v2_list = list(map(int, version2.split('.')))
+
+        for i in range(max(len(v1_list), len(v2_list))):
+            v1_val, v2_val = 0, 0
+            if i < len(v1_list): v1_val = v1_list[i]
+            if i < len(v2_list): v2_val = v2_list[i]
+
+            if v1_val < v2_val:
+                return -1
+            elif v1_val > v2_val:
+                return 1
+        return 0
+
+    def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
+        stop_list = ["!", "?", ",", ";", ".", ' ']
+        word_freq = {}
+
+        word_chars = []
+        max_word = (0, '')
+        for c in paragraph.lower():
+            if c in stop_list and word_chars:
+                word = ''.join(word_chars)
+                if word not in banned:
+                    word_freq[word] = word_freq.get(word, 0) + 1
+                    max_word = max(max_word, (word_freq[word], word))
+                word_chars = []
+            elif c == "'":
+                continue
+            elif c != ' ':
+                word_chars.append(c)
+        return max_word[1] if max_word[1] else ''.join(word_chars)
+
+    def reorderLogFiles(self, logs: list[str]) -> list[str]:
+        digit_logs = []
+        letter_logs = []
+
+        i = 0
+        while i < len(logs):
+            id_sep_index = str.index(logs[i], ' ')
+
+            if logs[i][id_sep_index+1:id_sep_index+2].isnumeric():
+                digit_logs.append(logs[i])
+            else:
+                if not letter_logs:
+                    letter_logs.append(logs[i])
+                else:
+                    log_data = logs[i][id_sep_index+1:]
+
+                    for j in range(len(letter_logs)):
+                        ll_id_sep_index = str.index(letter_logs[j], ' ')
+                        ll_log_data = letter_logs[j][ll_id_sep_index+1:]
+
+                        if log_data < ll_log_data:
+                            letter_logs.insert(j, logs[i])
+                            break
+                        elif log_data == ll_log_data:
+                            log_id = logs[i][:id_sep_index]
+                            ll_log_id = letter_logs[j][:ll_id_sep_index]
+
+                            if log_id < ll_log_id:
+                                letter_logs.insert(j, logs[i])
+                                break
+                        elif j == len(letter_logs) - 1:
+                            letter_logs.append(logs[i])
+                            break
+            i += 1
+        return letter_logs + digit_logs
+
+    def reorderLogFilesV2(self, logs: list[str]) -> list[str]:
+        def get_key(log):
+            _id, rest = log.split(" ", maxsplit=1)
+            return (0, rest, _id) if rest[0].isalpha() else (1, )
+
+        return sorted(logs, key=get_key)
+
+    def repeatedString(s, n):
+        pass
+
+    def numUniqueEmails(self, emails: list[str]) -> int:
+        def clean_email(email: str):
+            local, domain = email.split("@")
+            ignore_idx = local.find('+')
+            if ignore_idx >= 0:
+                local = local[:ignore_idx].replace('.', '')
+            else:
+                local = local.replace('.', '')
+
+            return local, domain
+
+        email_set = set()
+        for email in emails:
+            local, domain = clean_email(email)
+            email_set.add((local,domain))
+        print(email_set)
+        return len(email_set)
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s: return 0
+
+        char_idx = {}
+
+        l, r = 0, 0
+        max_len = 0
+        while r < len(s):
+            if s[r] in char_idx:
+                l = max(l, char_idx[s[r]])
+            char_idx[s[r]] = r
+            max_len = max(max_len, r-l+1)
+            r += 1
+        return max_len
+
+
+print(Solution().lengthOfLongestSubstring('abba'))
