fecc242b96892ba68c72c5cfee84f60e22e3daae
Taranjit Kooner <koonerts@gmail.com>
2020-12-29 09:48:15 -0500

    .

--- ace-tpi/trees-and-graphs/main.py
+++ ace-tpi/trees-and-graphs/main.py
@@ -31,35 +31,34 @@ def dfs_traversal_helper(g, source, visited):
     stack.push(source)
     visited[source] = True
     # Traverse while stack is not empty
-    while (stack.is_empty() is False):
+    while stack.is_empty() is False:
         # Pop a vertex/node from stack and add it to the result
         current_node = stack.pop()
         result += str(current_node)
         # Get adjacent vertices to the current_node from the array,
         # and if they are not already visited then push them in the stack
         temp = g.array[current_node].head_node
-        while (temp is not None):
-            if (visited[temp.data] is False):
+        while temp is not None:
+            if visited[temp.data] is False:
                 stack.push(temp.data)
                 # Visit the node
                 visited[temp.data] = True
             temp = temp.next_element
     return result, visited  # For the above graph it should return "12453"
 
+
 def dfs_traversal(g, source):
     result = ""
-    num_of_vertices = g.vertices
-    if num_of_vertices is 0:
-        return result
+    if g.vertices is 0: return result
     # A list to hold the history of visited nodes
     # Make a node visited whenever you enqueue it into queue
     visited = []
-    for i in range(num_of_vertices):
+    for i in range(g.vertices):
         visited.append(False)
     # Start from source
     result, visited = dfs_traversal_helper(g, source, visited)
     # visit remaining nodes
-    for i in range(num_of_vertices):
+    for i in range(g.vertices):
         if visited[i] is False:
             result_new, visited = dfs_traversal_helper(g, i, visited)
             result += result_new
@@ -68,9 +67,9 @@ def dfs_traversal(g, source):
 
 g = Graph(7)
 num_of_vertices = g.vertices
-if(num_of_vertices is 0):
+if num_of_vertices is 0:
     print("Graph is empty")
-elif(num_of_vertices < 0):
+elif num_of_vertices < 0:
     print("Graph cannot have negative vertices")
 else:
     g.add_edge(1, 2)
