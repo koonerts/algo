deb16115761a633c85c43ee9309a58aef38e6f4c
Taranjit Kooner <koonerts@gmail.com>
2021-01-10 21:01:59 -0500

    .

--- algo-expert/arrays.py
+++ algo-expert/arrays.py
@@ -318,4 +318,22 @@ def powerset(array):
     return q
 
 
+def threeNumberSort(array, order):
+    low, high = 0, len(array) - 1
+    i = 0
+    while i <= high:
+        if array[i] == order[0]:
+            array[i], array[low] = array[low], array[i]
+            # increment 'i' and 'low'
+            i += 1
+            low += 1
+        elif array[i] == order[1]:
+            i += 1
+        else:  # the case for array[i] == 2
+            array[i], array[high] = array[high], array[i]
+            # decrement 'high' only, after the swap the number at index 'i' could be 0, 1 or 2
+            high -= 1
+    return array
+
+
 print(powerset([1, 2, 3]))
