dcef89bcdd5c263680c04bd0e5c6648244e3462f
Taranjit Kooner <koonerts@gmail.com>
2021-04-08 20:50:35 -0400

    .

--- go-algo/main.go
+++ go-algo/main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go-algo/stripe"
+	"reflect"
+)
 
+func main() {
+	/*
+	parse_accept_language("en", ["en-US", "fr-CA", "fr-FR"])
+	returns: ["en-US"]
+
+	parse_accept_language("fr", ["en-US", "fr-CA", "fr-FR"])
+	returns: ["fr-CA", "fr-FR"]
+
+	parse_accept_language("fr-FR, fr", ["en-US", "fr-CA", "fr-FR"])
+	returns: ["fr-FR", "fr-CA"]
+	 */
+
+	// r1 := stripe.ParseAcceptLanguage("en", []string{"en-US", "fr-CA", "fr-FR"})
+	// r2 := stripe.ParseAcceptLanguage("fr", []string{"en-US", "fr-CA", "fr-FR"})
+	 r3 := stripe.ParseAcceptLanguage("fr-FR, fr, en", []string{"en-US", "fr-CA", "fr-FR", "en-CA"})
+	// r4 := stripe.ParseAcceptLanguage("", []string{"en-US", "fr-CA", "fr-FR"})
+	// r5 := stripe.ParseAcceptLanguage("en-", []string{"en-US", "fr-CA", "fr-FR"})
+
+	/*if reflect.DeepEqual(r1, []string{"en-US"}) {
+		fmt.Println(r1, true)
+	} else {
+		fmt.Println(r2, false)
+	}
+
+	if reflect.DeepEqual(r2, []string{"fr-CA", "fr-FR"}) {
+		fmt.Println(r2, true)
+	} else {
+		fmt.Println(r2, false)
+	}*/
+
+
+	if reflect.DeepEqual(r3, []string{"fr-FR", "fr-CA"}) {
+		fmt.Println(r3, true)
+	} else {
+		fmt.Println(r3, false)
+	}
+
+	/*
+	if reflect.DeepEqual(r4, []string{}) {
+		fmt.Println(r4, true)
+	} else {
+		fmt.Println(r4, false)
+	}
+
+	if reflect.DeepEqual(r5, []string{}) {
+		fmt.Println(r5, true)
+	} else {
+		fmt.Println(r5, false)
+	}*/
 }
--- go-algo/stripe/stripe.go
+++ go-algo/stripe/stripe.go
@@ -1,4 +1,39 @@
 package stripe
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
+// fr-FR, fr
+func ParseAcceptLanguage(reqHeaders string, acceptedHeaders []string) []string {
+	retList := make([]string, 0, len(acceptedHeaders))
+	retMap := map[string]bool{}
+	for _, ah := range acceptedHeaders {
+		exactMatchRegexp := regexp.MustCompile(ah)
+		matchedString := exactMatchRegexp.FindString(reqHeaders)
+		if matchedString != "" && !retMap[ah] {
+			retMap[ah] = true
+			retList = append(retList, ah)
+			continue
+		}
 
+		dashIdx := strings.Index(ah, "-")
+		langMatchRegexp := regexp.MustCompile(fmt.Sprintf("(^%s)(-\\w+?)?", ah[:dashIdx]))
+		fmt.Println(langMatchRegexp.FindStringSubmatch(reqHeaders))
+	}
+
+	return retList
+}
+
+func Contains(headers []string, header string) bool {
+	contains := false
+	for _, head := range headers {
+		if head == header {
+			contains = true
+			break
+		}
+	}
+	return contains
+}
