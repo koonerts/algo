ba650d3e4d9795323c0dd989a82e53de47afc607
Taranjit Kooner <koonerts@gmail.com>
2021-05-10 12:20:50 -0400

    .

---
+++ cpp-algo/cmake-build-debug/CMakeFiles/cpp_algo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_algo.dir/main.cpp.obj"
+  "cpp_algo.exe"
+  "cpp_algo.exe.manifest"
+  "cpp_algo.pdb"
+  "libcpp_algo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_algo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
